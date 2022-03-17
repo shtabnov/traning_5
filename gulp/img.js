3099cc425437efe59a17d90c23e441b3d11c367a
shtabnov <90615010+shtabnov@users.noreply.github.com>
2022-03-17 13:22:57 +0500

    async img

--- gulp/img.js
+++ gulp/img.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 
 
-module.exports = function minImg () {
+module.exports = async function minImg () {
     return gulp.src('src/images/*')
       .pipe(imagemin())
       .pipe(gulp.dest('build/images'))
