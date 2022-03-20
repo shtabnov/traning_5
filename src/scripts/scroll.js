320069a5200176e88658e0d40bf28f4703195647
Andrey <90615010+shtabnov@users.noreply.github.com>
2022-03-20 15:12:17 +0500

    add scroll to block

---
+++ src/scripts/scroll.js
@@ -0,0 +1,18 @@
+function scrollTo (element) {
+    window.scroll ({
+        left: 0,
+        top: element.offsetTop,
+        behavior: 'smooth'
+    })
+}
+
+const links = document.querySelectorAll('.header__link');
+
+for (let linkItem of links) {
+    linkItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        let target = event.target.getAttribute('href').substr(1);
+        scrollTo(document.querySelector('.'+ target));
+        
+    })
+}
